736ba9f788be1c5772ee9e2cc594eb875d3be442
viger <viger123@outlook.com>
2019-12-08 18:07:20 +0800

    减少等待时间到1秒 尝试增加漫游免认证

--- suda_WIFI_auto_login.py
+++ suda_WIFI_auto_login.py
@@ -13,6 +13,7 @@ headers = {'Host': '1d.suda.edu.cn',
            'Content-Length': '73',
            'Accept': 'application/json, text/javascript, */*; q=0.01',
            'Origin': 'http://1d.suda.edu.cn',
+           'Connection': 'keep-alive',
            'X-Requested-With': 'XMLHttpRequest',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3956.0 Safari/537.36 Edg/80.0.328.0',
            'DNT': '1',
@@ -32,7 +33,7 @@ while True:
         break
         # 当访问网关成功时, 输出登录情况, 退出程序
     else:
-        time.sleep(1000)
+        time.sleep(1)
         r = requests.request("post", "http://1d.suda.edu.cn/index.php/index/login", headers=headers, data=data)
         logging.debug("请连接到suda_WiFi, 一秒后自动重试")
         # 访问网关失败时, 等待一秒后重试
