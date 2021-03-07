01ef54361a6fb5393abb25111f2a802216baa108
viger201212 <viger123@outlook.com>
2021-03-07 10:59:13 +0800

    clean code

--- suda_WIFI_auto_login.py
+++ suda_WIFI_auto_login.py
@@ -4,11 +4,11 @@ import logging
 import time
 from typing import Dict
 import socket
-pcname = socket.getfqdn(socket.gethostname( ))
-pcip = socket.gethostbyname(pcname)
-pcip = socket.gethostbyname_ex(pcname)[2][1]
+# pcname = socket.getfqdn(socket.gethostname( ))
+# pcip = socket.gethostbyname(pcname)
+# pcip = socket.gethostbyname_ex(pcname)[2][1]
 
-print(pcip)
+# print(pcip)
 
 import requests
 
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     # config area
-    # baseurl = "http://10.9.1.3:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=%2C0%2C1827406006&user_password=llhw201212&wlan_user_ip=10.70.87.222&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=221.178.234.23&wlan_ac_name=&jsVersion=3.3.3&v=6050"
     with open('config.json') as fp:
         config = json.load(fp=fp)
     password = config["password"]
@@ -54,4 +53,3 @@ if __name__ == "__main__":
             time.sleep(1)
             logging.debug("请连接到suda_WiFi, 一秒后自动重试")
             # 访问网关失败时, 等待一秒后重试
-    input("Press enter to exit ;)")
